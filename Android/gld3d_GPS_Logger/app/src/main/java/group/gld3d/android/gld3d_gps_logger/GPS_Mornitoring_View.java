18d14368d5fbad02807347d7ddc7b8bfe13643af
바른생활심원일 <red112@naver.com>
2016-09-25 00:05:34 +0900

    GPS 정보를 화면에 출력

--- Android/gld3d_GPS_Logger/app/src/main/java/group/gld3d/android/gld3d_gps_logger/GPS_Mornitoring_View.java
+++ Android/gld3d_GPS_Logger/app/src/main/java/group/gld3d/android/gld3d_gps_logger/GPS_Mornitoring_View.java
@@ -5,13 +5,24 @@ import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class GPS_Mornitoring_View extends AppCompatActivity  implements LocationListener {
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class GPS_Mornitoring_View extends AppCompatActivity implements LocationListener {
     private LocationManager locationManager;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +30,39 @@ public class GPS_Mornitoring_View extends AppCompatActivity  implements Location
         setContentView(R.layout.activity_gps__mornitoring__view);
 
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 1, this);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, this);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
     public void onLocationChanged(Location location) {
-        String msg = "New Latitude: " + location.getLatitude()
-                + "New Longitude: " + location.getLongitude();
 
+        TextView textView_Lat = (TextView) findViewById(R.id.textView_Lat);
+        TextView textView_Lon = (TextView) findViewById(R.id.textView_Lon);
+        TextView textView_Alt = (TextView) findViewById(R.id.textView_Alt);
+        TextView textView_Accuracy = (TextView) findViewById(R.id.textView_Accuracy);
+        TextView textView_Update = (TextView) findViewById(R.id.textView_Update);
+
+        String strLat = "Lat.: " + location.getLatitude();
+        String strLon = "Lon.: " + location.getLongitude();
+        String strAlt = "Alt.: " + location.getAltitude();
+        String strAccrc = "Accuracy : " + location.getAccuracy();
+//        String strUpdt =
+
+        textView_Lat.setText(strLat);
+        textView_Lon.setText(strLon);
+        textView_Alt.setText(strAlt);
+        textView_Accuracy.setText(strAccrc);
+        //textView_Update.setText();
+
+        /*
+        String msg = "Lat.: " + location.getLatitude()
+                + "Lon: " + location.getLongitude();
         Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+        */
+
     }
 
     @Override
@@ -50,4 +85,43 @@ public class GPS_Mornitoring_View extends AppCompatActivity  implements Location
     }
 
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "GPS_Mornitoring_View Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://group.gld3d.android.gld3d_gps_logger/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "GPS_Mornitoring_View Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://group.gld3d.android.gld3d_gps_logger/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
