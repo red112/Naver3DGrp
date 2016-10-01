b0383ccc9e94b87329abd04da8dee90f8ac92d9d
바른생활심원일 <red112@naver.com>
2016-10-01 22:27:26 +0900

    일시 문자열 출력

--- Android/gld3d_GPS_Logger/app/src/main/java/group/gld3d/android/gld3d_gps_logger/GPS_Mornitoring_View.java
+++ Android/gld3d_GPS_Logger/app/src/main/java/group/gld3d/android/gld3d_gps_logger/GPS_Mornitoring_View.java
@@ -16,6 +16,9 @@ import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class GPS_Mornitoring_View extends AppCompatActivity implements LocationListener {
     private LocationManager locationManager;
     /**
@@ -51,17 +54,26 @@ public class GPS_Mornitoring_View extends AppCompatActivity implements LocationL
         String strAccrc = "Accuracy : " + location.getAccuracy();
 //        String strUpdt =
 
+        //Time
+        StringBuilder   strBuilder_curTime = new StringBuilder();
+        Calendar        calenar_ = new GregorianCalendar();
+
+        strBuilder_curTime.append(String.format("Updated : %d_%d_%d_%d_%d_%d",
+                calenar_.get(Calendar.YEAR),
+                calenar_.get(Calendar.MONTH)+1,
+                calenar_.get(Calendar.DAY_OF_MONTH),
+                calenar_.get(Calendar.HOUR_OF_DAY),
+                calenar_.get(Calendar.MINUTE),
+                calenar_.get(Calendar.SECOND)
+                ));
+
         textView_Lat.setText(strLat);
         textView_Lon.setText(strLon);
         textView_Alt.setText(strAlt);
         textView_Accuracy.setText(strAccrc);
-        //textView_Update.setText();
+        textView_Update.setText(strBuilder_curTime);
 
-        /*
-        String msg = "Lat.: " + location.getLatitude()
-                + "Lon: " + location.getLongitude();
-        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
-        */
+
 
     }
 
